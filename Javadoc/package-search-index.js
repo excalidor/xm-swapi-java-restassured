53f9ced798ac4772af190e3be97034cfb1d5f4a4
Doru Liviu <doru75c@yahoo.com>
2023-12-22 01:10:37 +0200

    adding Jvadoc

---
+++ Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"starter.api"},{"l":"starter.models"},{"l":"starter.testImplementations"},{"l":"starter.testsuite"},{"l":"starter.Utils"}];updateSearchResults();
